7065edf5cca11caccc2a0f5c33f09b484224ecf3
Eugene Min <mineugene@users.noreply.github.com>
2020-09-02 15:37:40 -0400

    renamed (person,seller)->(client,business)

--- tests/entities/test_business.py
+++ tests/entities/test_business.py
@@ -1,15 +1,15 @@
-from receipt_tracker.entities.seller import Seller
+from receipt_tracker.entities.business import Business
 
 
 def test_seller_init():
-    seller = Seller(id=1, name='Steam')
+    seller = Business(id=1, name='Steam')
 
     assert seller.id == 1
     assert seller.name == 'Steam'
 
 
 def test_seller_init_from_dict():
-    seller = Seller.from_dict(
+    seller = Business.from_dict(
         {
             'id': 1,
             'name': 'Steam'
@@ -26,5 +26,5 @@ def test_seller_to_dict():
         'name': 'Steam'
     }
 
-    seller = Seller.from_dict(seller_dict)
+    seller = Business.from_dict(seller_dict)
     assert seller.to_dict() == seller_dict
