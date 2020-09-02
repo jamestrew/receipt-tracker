12d4d097525a0b53cbe47dd0cad6256980ab05ed
trewjames <j.trew10@gmail.com>
2020-09-02 16:32:43 -0400

    Implement basic add new client form.

--- receipt_tracker/flask/__init__.py
+++ receipt_tracker/flask/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+# Secret key required to use CSRF
+# Probably should use env var?
+app.config['SECRET_KEY'] = '2702a6eab639bd94773edb5af1875c53'
 
 from receipt_tracker.flask import routes  # noqa
--- receipt_tracker/flask/routes.py
+++ receipt_tracker/flask/routes.py
@@ -1,6 +1,7 @@
 import flask
 
 from receipt_tracker.flask import app
+from receipt_tracker.flask.forms import ClientForm
 
 
 @app.route("/")
@@ -12,3 +13,14 @@ def home():
 @app.route("/stats")
 def stats():
     return flask.render_template('stats.html')
+
+
+@app.route("/new_client", methods=['GET', 'POST'])
+def new_client():
+    form = ClientForm()
+
+    if form.validate_on_submit():
+        flask.flash('New buyer added!', 'success')
+        return flask.redirect(flask.url_for('home'))
+
+    return flask.render_template('new_client.html', form=form)
