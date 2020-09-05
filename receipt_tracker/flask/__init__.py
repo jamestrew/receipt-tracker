62285757d9e79c22fbdd455ca07cd3b73c4a360c
trewjames <j.trew10@gmail.com>
2020-09-05 16:51:32 -0400

    Fix circular import in receipt_track.flask package.

--- receipt_tracker/flask/__init__.py
+++ receipt_tracker/flask/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_datepicker import datepicker
+from receipt_tracker.repo.database import init_db, db_session
 
-from receipt_tracker.flask import routes  # noqa
-from receipt_tracker.repo.database import db_session, init_db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2702a6eab639bd94773edb5af1875c53'
@@ -11,3 +10,6 @@ Bootstrap(app)
 datepicker(app)
 db = db_session
 init_db()
+
+
+from receipt_tracker.flask import routes  # noqa
