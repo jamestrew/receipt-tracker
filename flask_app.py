e721534d00861049f4b91f72b0bc5a11693305b3
trewjames <j.trew10@gmail.com>
2020-09-08 17:10:16 -0400

    Move session shutdown to flask package (resolve sqlite error). Sqlite ProgrammingError.

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,6 @@
-from receipt_tracker.flask import app, session
+from receipt_tracker.flask import app
 
 if __name__ == "__main__":
     # Start the flask application with development settings.
 
     app.run(debug=True)
-
-    @app.teardown_appcontext
-    def shudown_db_session(exeception=None):
-        """Dispose of the current Session, if present.
-
-        This will first call Session.close() method on the current Session, which releases any existing transactional/connection resources still being held; transactions specifically are rolled back. The Session is then discarded. Upon next usage within the same scope, the scoped_session will produce a new Session object.
-
-        """
-        session.remove()
--- receipt_tracker/flask/__init__.py
+++ receipt_tracker/flask/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_datepicker import datepicker
 
 from receipt_tracker.repo.sql_repo import SQLRepo
-from receipt_tracker.repo.sql_config import DEV_CONFIG
+from receipt_tracker.repo.sql_config import SQL_DEV_CONFIG
 
 
 app = Flask(__name__)
@@ -11,8 +11,17 @@ app.config['SECRET_KEY'] = '2702a6eab639bd94773edb5af1875c53'
 Bootstrap(app)
 datepicker(app)
 
-repo = SQLRepo(DEV_CONFIG)
+repo = SQLRepo(SQL_DEV_CONFIG)
 session = repo.init_db()
+
+
+@app.teardown_appcontext
+def shudown_db_session(exeception=None):
+    """Dispose of the current Session, if present.
+
+    This will first call Session.close() method on the current Session, which releases any existing transactional/connection resources still being held; transactions specifically are rolled back. The Session is then discarded. Upon next usage within the same scope, the scoped_session will produce a new Session object.
+    """
+    session.remove()
 
 
 from receipt_tracker.flask import routes  # noqa
