634278a4d2fe2dca37399de7e9dfe7932b383567
trewjames <j.trew10@gmail.com>
2020-09-16 16:12:28 -0400

    Revert deletion of UC:get_entities.

--- receipt_tracker/use_cases/list_uc.py
+++ receipt_tracker/use_cases/list_uc.py
@@ -1,6 +1,12 @@
 from receipt_tracker.entities.results import Table
 
 
+def get_entities(repo, table, attr=None):
+    if attr is not None and attr not in table.__dict__:
+        raise KeyError(f'{table} does not contain the {attr} attribute')
+    return repo.list_entities(table, attr)
+
+
 def create_table(repo, table, fields=[], table_title=None):
 
     return Table(
